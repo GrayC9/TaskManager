a501aa29c3c41f8a8e7a7d6c4f892079d9df365d
Danila Manakov <Telegraphrabbit@yandex.ru>
2024-08-19 00:31:18 +0300

    1 task

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,17 @@
 package main
 
 import (
+	"Gazprome_TaskManager/internal_1/handlers"
+	"fmt"
 	"log"
 	"net/http"
-
-	"Gazprome_TaskManager/internal/handlers"
-	"Gazprome_TaskManager/internal/services"
-	"Gazprome_TaskManager/internal/storage"
-	"Gazprome_TaskManager/pkg"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	database, err := pkg.ConnectDB()
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
-	defer database.Close()
+	http.HandleFunc("/", handlers.FormHandler)
 
-	taskRepo := &storage.TaskRepository{DB: database}
-	taskService := &services.TaskService{Repo: taskRepo}
-	taskHandler := &handlers.TaskHandler{Service: taskService}
-
-	r := mux.NewRouter()
-
-	r.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
-	r.HandleFunc("/tasks", taskHandler.GetTasks).Methods("GET")
-	r.HandleFunc("/tasks/{id}", taskHandler.UpdateTaskStatus).Methods("PUT")
-	r.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
-
-	log.Println("Server is running on port 8080...")
-	err = http.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Fatal("Failed to start server:", err)
+	fmt.Println("Сервер запущен на порту 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Ошибка запуска сервера: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module Gazprome_TaskManager
 
 go 1.22
-
-require (
-    github.com/gorilla/mux v1.8.1
-    github.com/go-sql-driver/mysql v1.8.1
-)
