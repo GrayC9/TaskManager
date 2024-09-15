5871c04714b0f0794bccb2da6a7b31d1124e8d5e
Danila Manakov <Telegraphrabbit@yandex.ru>
2024-09-15 16:52:22 +0300

    bug cycle

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/GrayC9/TaskManager/internal/config"
-	"github.com/GrayC9/TaskManager/internal/handlers"
-	"github.com/GrayC9/TaskManager/internal/storage"
+	"github.com/GrayC9/TaskManager/internal/taskmanager"
 )
 
 func main() {
-	cfg := config.LoadConfig()
-
-	err := storage.InitDB(cfg.DB.DSN)
-	if err != nil {
-		log.Fatalf("Ошибка подключения к базе данных: %v", err)
-	}
-	defer storage.CloseDB()
-
-	http.HandleFunc("/", handlers.TaskHandler)
-
-	fmt.Println("Сервер запущен на порту", cfg.Server.Address)
-	if err := http.ListenAndServe(cfg.Server.Address, nil); err != nil {
-		log.Fatalf("Ошибка запуска сервера: %v", err)
-	}
+	taskmanager.Run()
 }
